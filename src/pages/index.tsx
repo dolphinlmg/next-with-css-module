cb4daf6203f0981d70d182a54454dd9cedf32f88
dolphinlmg <dolphinlmg@naver.com>
2023-09-21 13:56:25 +0900

    Create index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,22 @@
+import Button from '@/components/Button';
+import style from '@/styles/index.module.scss';
+
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <div className={style.header}>
+        <ul>
+          <li>
+            <a>Home</a>
+          </li>
+          <li>
+            <a>About</a>
+          </li>
+        </ul>
+      </div>
+      <div className={style['button-container']}>
+        <Button>Hello world!</Button>
+      </div>
+    </>
+  );
 }
